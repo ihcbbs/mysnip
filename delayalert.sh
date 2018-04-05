04d65adc60fe291f517b75026f1b0cfa053503a6
ihcbbs <doruison@gmail.com>
2018-04-05 11:53:07 +0800

    Add files via upload

--- delayalert.sh
+++ delayalert.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/bash
-traceroute -T -A -p 443 ah.189.cn > /web/alert.txt
+traceroute -T -A -p 443 ah.189.cn > /web/temp
 
 
 
 
 Issue=0
-Issue=$(traceroute -T -A -p 443 ah.189.cn |grep  -m 1 4134| cut -d ']' -f 3|cut -d '.' -f 1 )
+Issue=$(traceroute -T -A -p 443 ah.189.cn |grep  -m 1 4134| cut -d ']' -f 3|cut -d '.' -f 1)
 if [ $Issue -gt 91  ];then
-echo "The delay is a problem" >> /web/alert.txt
+echo "High delay." >> /web/temp
 fi
 if [ $Issue -lt 91  ];then
- echo "No problem" >> /web/alert.txt
+ echo "Low delay." >> /web/temp
+fi
+Issue=$(traceroute -T -A -p 443 ah.189.cn |grep  -m 1 4134| cut -d ']' -f 3|cut -d '.' -f 1)
+if [ $Issue -gt 91  ];then
+echo "High delay." >> /web/temp
+fi
+if [ $Issue -lt 91  ];then
+ echo "Low delay." >> /web/temp
 fi
 
-
-
+mv /web/temp /web/alert.txt
 
 chown -R www-data:www-data /web
 exit 0
