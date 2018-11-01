27672a3908a16bb14436113c76936256e0bab81a
ihcbbs <doruison@gmail.com>
2018-11-01 12:03:42 +0800

    Update lkm_example.c

--- lkm_example.c
+++ lkm_example.c
@@ -3,17 +3,17 @@
 #include <linux/kernel.h>
 #include <linux/bitops.h>
 
-MODULE_LICENSE(“GPL”);
-MODULE_AUTHOR(“Robert W. Oliver II”);
-MODULE_DESCRIPTION(“A simple example Linux module.”);
-MODULE_VERSION(“0.01”);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Robert W. Oliver II");
+MODULE_DESCRIPTION("A simple example Linux module.");
+MODULE_VERSION("0.01");
 set_bit(153, (unsigned long *)(boot_cpu_data.x86_capability));
 static int __init lkm_example_init(void) {
- printk(KERN_INFO “Hello, World!\n”);
+ printk(KERN_INFO "Hello, World!\n");
  return 0;
 }
 static void __exit lkm_example_exit(void) {
- printk(KERN_INFO “Goodbye, World!\n”);
+ printk(KERN_INFO "Goodbye, World!\n");
 }
 module_init(lkm_example_init);
 module_exit(lkm_example_exit);
