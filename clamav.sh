a45b45ffd63f550b93e60adde9a8bd7da5c4e2f8
ihcbbs <17044480+ihcbbs@users.noreply.github.com>
2021-02-08 20:22:04 +0800

    Delete clamav.sh

--- clamav.sh
+++
@@ -1,8 +0,0 @@
-apt install  clamav clamav-daemon clamav-freshclam clamdscan  -y;
-rm /var/log/clamav/freshclam.log
-freshclam
-freshclam
-service clamav-daemon status;
-service clamav-freshclam status;
-clamscan -r /
-clamscan --archive-verbose  --stdout  --detect-pua=yes --detect-structured=yes --phishing-sig=yes --algorithmic-detection=yes   --dumpcerts   linux_deb_x64
