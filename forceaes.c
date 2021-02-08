9443fb06459b510335d115e3704f50e1ca9ea176
ihcbbs <17044480+ihcbbs@users.noreply.github.com>
2021-02-08 20:23:27 +0800

    Delete forceaes.c

--- forceaes.c
+++
@@ -1,9 +0,0 @@
-#include <linux/bitops.h>
-set_bit(153, (unsigned long *)(boot_cpu_data.x86_capability));
-
-module_init(forceases_register);
-module_exit(forceases_unregister);
-
-
-MODULE_LICENSE("Dual BSD/GPL");
-MODULE_DESCRIPTION("forceaes");
