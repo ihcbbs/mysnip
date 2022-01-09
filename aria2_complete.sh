a23b0851148ae9708ab6bcd867a7d41157c13248
ihcbbs <17044480+ihcbbs@users.noreply.github.com>
2022-01-09 13:48:13 +0800

    Add files via upload

--- aria2_complete.sh
+++ aria2_complete.sh
@@ -7,9 +7,12 @@ if [ $2 -eq 0 ]; then
 fi
 if [ $2 -eq 1 ]; then
     /usr/bin/mv -f $path /data/aria2/adown/complete/
+    /usr/bin/mv -f "$path".aria2 /data/aria2/adown/complete/
     /usr/bin/echo "$path".aria2 >>/.aria2/0.log
 
     exit 0
 fi
 /usr/bin/mv -f $parents /data/aria2/adown/complete/
+/usr/bin/mv -f "$parents".aria2 /data/aria2/adown/complete/
 /usr/bin/echo "$parents".aria2 >>/.aria2/0.log
+#/usr/bin/mv -f "$parents".aria2 /data/aria2/adown/complete/
